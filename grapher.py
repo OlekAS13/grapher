e149a133679ced9aff81ab18caeb751888d64af4
Olek Stachura <133813772+OlekAS13@users.noreply.github.com>
2024-11-11 15:15:46 +0100

    Add files via upload

--- grapher.py
+++ grapher.py
@@ -1,87 +1,144 @@
 from tkinter import *
 
-print("y = a x^2 + b x + c")
-
 sx = 20
 sy = 20
 
-#inputs
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
+def askFuncType():
+    global funcType
 
-print("y =", a, "x^2 +", b, "x +", c)
+    funcType = str(input("Function type (linear: y = a x + b, quadratical: y = a x^2 + b x + c): L/Q: "))
 
+def askScale():
+    global sx, sy
 
+    sx = int(input("Scale X (How many digits will be between the spacers; 20 is default): INT: "))
+    sy = int(input("Scale Y (How many digits will be between the spacers; 20 is default): INT: "))
 
+def drawGraph():
+    global xAxis, yAxis, newScale
 #main grapher window
-graph = Tk()
+    graph = Tk()
 
-equation = "y = {} x^2 + {} x + {}".format(a, b ,c)
-scaletext = "sx = {}, sy = {}".format(sx, sy)
+    if funcType == "Q":
+        equation = "y = {} x^2 + {} x + {}".format(a, b ,c)
+    elif funcType == "L":
+        equation = "y = {} x + {}".format(a, b)
 
-graph.title("Grapher [OlekAS13] | " + equation)
+    scaletext = "sx = {}, sy = {}".format(sx, sy)
 
-canv = Canvas(graph, width=801, height=801, background="white")
+    graph.title("Grapher [OlekAS13] | " + equation)
 
-#sliderX = Scale(graph, from_=0, to=100)
-#sliderY = Scale(graph, from_=0, to=100)
-#sliderX.pack()
-#sliderY.pack()
+    canv = Canvas(graph, width=801, height=801, background="white")
 
-canv.create_text(796, 796, text=equation, anchor="se", justify="right")
-canv.create_text(5, 796, text=scaletext, anchor="sw", justify="left")
+    #sliderX = Scale(graph, from_=0, to=100)
+    #sliderY = Scale(graph, from_=0, to=100)
+    #sliderX.pack()
+    #sliderY.pack()
 
-xAxis = canv.create_line(0, 400, 800, 400, fill="black")
-yAxis = canv.create_line(400, 0, 400, 800, fill="black")
+    canv.create_text(796, 796, text=equation, anchor="se", justify="right")
+    canv.create_text(5, 796, text=scaletext, anchor="sw", justify="left")
 
-#x cosmetics
-canv.create_text(795, 400, text=">")
-canv.create_text(795, 415, text="x", anchor="n", justify="left")
+    xAxis = canv.create_line(0, 400, 800, 400, fill="black")
+    yAxis = canv.create_line(400, 0, 400, 800, fill="black")
 
-#y cosmetics
-canv.create_text(400, 7, text="^")
-canv.create_text(385, 5, text="y")
+    #x cosmetics
+    canv.create_text(795, 400, text=">")
+    canv.create_text(795, 415, text="x", anchor="n", justify="left")
 
-
-#middle
-canv.create_text(385, 415, text="0")
-
-#x 1
-canv.create_text(400 + sx, 415, text="1")
-#y 1
-canv.create_text(385, 400 - sy, text="1")
+    #y cosmetics
+    canv.create_text(400, 7, text="^")
+    canv.create_text(385, 5, text="y")
 
 
+    #middle
+    canv.create_text(385, 415, text="0")
 
-for xSpace in range(0, 800, sx):
-    canv.create_line(xSpace, 395, xSpace, 405, fill="black")
+    #x 1
+    canv.create_text(400 + sx, 415, text="1")
+    #y 1
+    canv.create_text(385, 400 - sy, text="1")
 
-for ySpace in range(0, 800, sy):
-    canv.create_line(395, ySpace, 405, ySpace, fill="black")
+    #spacers
+    for xSpace in range(0, 800, sx):
+        canv.create_line(xSpace, 395, xSpace, 405, fill="black")
+
+    for ySpace in range(0, 800, sy):
+        canv.create_line(395, ySpace, 405, ySpace, fill="black")
+
+    #buttons
+    #Button(graph, text="New scale", comman=askScale, width=10, background="white", highlightbackground="gray",).pack()
 
 
-i = 0
-x = -400 / sx
+    i = 0
+    x = -400 / sx
 
 
+    if funcType == "Q":
+        while i <= 801:
+            y = a * pow(x, 2) + b * x + c
 
-while i <= 801:
+            px = x * sx + 400
+            py = -y * sy + 400
+            
+            canv.create_rectangle((px, py) * 2, outline="blue")
 
-    y = a * pow(x, 2) + b * x + c
+            x += 0.01
+            i += 0.01
+        
+        canv.pack()
 
-    px = x * sx + 400
-    py = -y * sy + 400
-    
-    canv.create_rectangle((px, py) * 2, outline="blue")
+    elif funcType == "L":
+        while i <= 801:
+            y = a * x + b
 
-    x += 0.01
-    i += 0.01
+            px = x * sx + 400
+            py = -y * sy + 400
+            
+            canv.create_rectangle((px, py) * 2, outline="blue")
 
-    #print(i)
+            x += 0.01
+            i += 0.01
+        
+        canv.pack()
 
-canv.pack()
-graph.mainloop()
+    graph.mainloop()
+
+
+def main():
+    global a, b, c
+
+    askFuncType()
+    askScale()
+
+    if funcType == "Q":
+        print("y = a x^2 + b x + c")
+
+        #inputs
+        a = float(input("a = "))
+        b = float(input("b = "))
+        c = float(input("c = "))
+
+        print("y =", a, "x^2 +", b, "x +", c)
+
+        drawGraph()
+        
+    elif funcType == "L":
+        print("y = a x + b")
+
+        #inputs
+        a = float(input("a = "))
+        b = float(input("b = "))
+
+        print("y =", a, "x +", b)
+
+        drawGraph()
+
+    else:
+        print("Please specify a correct function type.")
+        main()
+
+main()
+
 
 #By OlekAS13
 #Nov 11 2024 1:52
